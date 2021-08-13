ca5386b07ed9d92967e17fa66b20016b6fb785b7
faisal.ihsanulfikri <faisalihsanulfikri@gmail.com>
2021-08-13 15:07:02 +0700

    [CHG] remove unused

--- 2.js
+++ 2.js
@@ -1,9 +1,4 @@
 function reverseCharString(binaries) {
-  /** validation */
-  if(!Array.isArray(binaries)) return 'invalid data';
-  let totalInvalidValues = binaries.filter((binary) => ![0,1].includes(binary)).length;
-  if (totalInvalidValues > 0) return 'invalid data';
-
   return str ? reverseCharString(str.substr(1)) + str[0] : str;
 }
 
